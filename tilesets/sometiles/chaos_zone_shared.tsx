ae8cce400b70e2fa59f034d28150243a12c43cef
Robert van den Breemen <robert@vandenbreemen.net>
2022-01-22 10:28:48 +0100

    Revert "some mate"
    This reverts commit 66b7b98a7965651dedc1186cdd14cde5038ad9af.

--- tilesets/sometiles/chaos_zone_shared.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="chaos_zone_shared" tilewidth="32" tileheight="32" tilecount="2048" columns="32">
- <image source="../tilesets/sometiles/chaos_zone_shared.png" width="1024" height="2048"/>
-</tileset>
--- tilesets/sometiles/tilesets_fsm_rm2k3_para_vx_ace__set_hospital_by_milkian_darwnhe.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="tilesets_fsm_rm2k3_para_vx_ace__set_hospital_by_milkian_darwnhe" tilewidth="32" tileheight="32" tilecount="256" columns="16">
- <image source="../tilesets/sometiles/tilesets_fsm_rm2k3_para_vx_ace__set_hospital_by_milkian_darwnhe.png" width="512" height="512"/>
-</tileset>
