fba04e743a3675163fd93107df5f55fd17e497b7
Robert van den Breemen <robert@vandenbreemen.net>
2022-01-29 20:08:43 +0100

    decorating

---
+++ tilesets/sometiles/fosdemlogo2016.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="fosdemlogo2016" tilewidth="32" tileheight="32" tilecount="1681" columns="41">
+ <image source="fosdemlogo2016.png" trans="000000" width="1314" height="1324"/>
+</tileset>
