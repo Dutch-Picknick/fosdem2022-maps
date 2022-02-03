0fe0b0e50c478077b8e28c546e68cbbf005adbc3
Robert van den Breemen <robert@vandenbreemen.net>
2022-02-04 00:41:43 +0100

    adding some tiles

--- tilesets/sometiles/fosdemlogo2016-128x128.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="fosdemlogo2016-128x128" tilewidth="32" tileheight="32" tilecount="16" columns="4">
- <image source="fosdemlogo2016-128x128.png" trans="000000" width="128" height="129"/>
-</tileset>
--- tilesets/sometiles/fosdemlogo2016-256x256.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="fosdemlogo2016-256x256" tilewidth="32" tileheight="32" tilecount="56" columns="7">
- <image source="fosdemlogo2016-256x256.png" trans="000000" width="254" height="256"/>
-</tileset>
